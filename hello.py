7892f5fc79182867b1e95a0ea7f333d9381ef379
Matthew Barber <quitesimplymatt@gmail.com>
2021-01-05 12:28:07 +0000

    basic game routing

--- hello.py
+++ hello.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+from peewee import *
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+db = SqliteDatabase('sandbox.db')
+
+class Game(Model):
+    id = IntegerField(primary_key=True)
+    slug = CharField()
+    title = CharField()
+    developer = CharField()
+    date = DateField()
+    category = CharField()
+    description = CharField()
+    tags = CharField()
+    rating = FloatField()
+    nratings = IntegerField()
+    downloads = IntegerField()
+    version = IntegerField()
+
+    class Meta:
+        database = db # this model uses the "people.db" database.
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/game/<game_id>")
+def hello(game_id):
+    game = Game.get(Game.id == int(game_id))
+    return render_template("game.html", game=game)
